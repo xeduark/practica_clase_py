45004c8dd0d26371a7fb426c22ec96230433df51
Anderson <aaalzate5@misena.edu.co>
2024-08-30 19:30:13 -0500

    cambio anderson

--- prueba.py
+++ prueba.py
@@ -1,5 +1,12 @@
+
 # Cambios Edward
-if condition:
-    print("Verdadero")
-else:
-    print("Falso")
+def prueba():
+    if False:
+       
+        print("Esta parte nunca se ejecutará.")
+    else:
+       
+        print("La condición es False, así que esta parte se ejecutará.")
+
+
+prueba()
