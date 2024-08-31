a08a6bd462fbea012813b55444c83abe01168f62
jonny londoño <johnnylon76@gmail.com>
2024-08-30 19:28:34 -0500

    cambio3

--- prueba.py
+++ prueba.py
@@ -5,13 +5,10 @@ else:
     print("Falso")
 
    
-print(name[0])
-print(name[1])
-print(name[2])
 
 name = 'carla marcela'
 
 print(name[0])
 print(name[1])
 print(name[2])
-print(namr[3])
+print(name[3])
