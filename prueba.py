dcaedaaccbff007769719893aa97e293acab55b0
jonny londoño <johnnylon76@gmail.com>
2024-08-30 19:18:52 -0500

    cambio1

--- prueba.py
+++ prueba.py
@@ -3,3 +3,15 @@ if condition:
     print("Verdadero")
 else:
     print("Falso")
+
+   
+print(name[0])
+print(name[1])
+print(name[2])
+
+name = 'carla marcela'
+
+print(name[0])
+print(name[1])
+print(name[2])
+print(namr[3])
